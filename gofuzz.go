47a1a582bb8cbe445ee592555b7e60db8d2cc12f
Noel Cower <ncower@gmail.com>
2018-07-04 14:13:28 -0700

    Add a FuzzWalker function
    Just used to pass parsed fuzzer input to the walker.

--- gofuzz.go
+++ gofuzz.go
@@ -4,6 +4,7 @@ package codf // import "go.spiff.io/codf"
 
 import (
 	"bytes"
+	"fmt"
 )
 
 func Fuzz(b []byte) (rc int) {
@@ -21,4 +22,47 @@ func FuzzLexer(b []byte) (rc int) {
 			return
 		}
 	}
+}
+
+type fuzzWalker struct{}
+
+func (w *fuzzWalker) Statement(st *Statement) error {
+	switch st.Name() {
+	case "server_name",
+		"listen",
+		"proxy_pass",
+		"add_header",
+		"user",
+		"daemon",
+		"log_format",
+		"acces_log",
+		"error_log":
+		return nil
+	default:
+		return fmt.Errorf("invalid statement name: %s", st.Name())
+	}
+}
+
+func (w *fuzzWalker) EnterSection(sec *Section) (Walker, error) {
+	switch sec.Name() {
+	case "http",
+		"server",
+		"location",
+		"upstream",
+		"stream":
+		return w, nil
+	default:
+		return nil, fmt.Errorf("invalid section name: %s", sec.Name())
+	}
+}
+
+func FuzzWalker(b []byte) (rc int) {
+	lex := NewLexer(bytes.NewReader(b))
+	p := NewParser()
+	if err := p.Parse(lex); err != nil {
+		// No panic, but can't use the document
+		return 0
+	}
+	Walk(p.Document(), new(fuzzWalker))
+	return 0
 }
